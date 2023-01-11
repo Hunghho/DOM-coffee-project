4efe4a46761acdd8aafd3c1dcf9816cf9c63a138
Titus Williams <wtwilliams310@gmail.com>
2023-01-11 11:05:31 -0600

    des: formatted coffee placement and changed table to divs

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="drop-outer-coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
@@ -29,6 +29,72 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+
+//------------- First Input Field----------------
+
+const search = document.querySelector('#coffee-name');
+const output = document.querySelector('#coffees');
+
+
+window.addEventListener('DOMContentLoaded', loadTable);
+
+
+search.addEventListener('input', filter)
+
+
+function loadTable(){
+    let html = `<div class="outer-coffee">`;
+    // tableHeader.forEach(header => html+= `<th> ${header.toUpperCase()} </th>`)
+    // html += `<tr>`
+    coffees.forEach(coffee => {
+        html+= `
+        <div class="inner-coffee">
+            <h1>${coffee.name}</h1>
+            <p>${coffee.roast}</p>
+        </div>
+            `
+    })
+
+    html += `</div>`
+    
+
+    output.innerHTML = html;
+}
+
+
+function filter(e){
+    // console.log(e.target.value);
+    let results;
+    let html = "";
+
+    results = coffees.filter(coffee => coffee.name.toLowerCase().includes(e.target.value.toLowerCase()))
+
+    console.log(results);
+
+    if (results.length > 0){
+        html = `<div class="outer-coffee">`;
+        // html += `<tr>`
+        results.forEach(coffee => {
+            html+= `
+            <div class="inner-coffee">
+                <h1>${coffee.name}</h1>
+                <p>${coffee.roast}</p>
+            </div>
+            `
+        })
+
+        html += `</div>`
+    } else {
+        html = `<div class="no-itme">Item Not Found </div>`
+    }
+
+    output.innerHTML = html 
+}
+
+// Input End-----
+
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
